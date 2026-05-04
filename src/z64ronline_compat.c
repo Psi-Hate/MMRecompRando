bb7b6c31ba5dea9ed44e279bf1a1ec7bbaf35ffc
Psi-Hate <Psi-Hate@users.noreply.github.com>
2026-05-04 14:52:17 -0500

    push player slot on send location event

--- src/z64ronline_compat.c
+++ src/z64ronline_compat.c
@@ -3,9 +3,9 @@
 
 #include "apcommon.h"
 
-RECOMP_DECLARE_EVENT(Z64ROnline_onRandoLocationSent(u32 location));
+RECOMP_DECLARE_EVENT(Z64ROnline_onRandoLocationSent(u32 location, int slot));
 
 void Z64ROnline_rando_send_location(u32 location) {
     rando_send_location(location);
-    Z64ROnline_onRandoLocationSent(location);
+    Z64ROnline_onRandoLocationSent(location, rando_get_own_slot_id());
 }
