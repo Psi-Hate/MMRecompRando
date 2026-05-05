977b4305447d86564a249d94dd2eab61ebd60a70
Psi-Hate <Psi-Hate@users.noreply.github.com>
2026-05-04 20:19:37 -0500

    Revert "push player slot on send location event"
    This reverts commit bb7b6c31ba5dea9ed44e279bf1a1ec7bbaf35ffc.

--- src/z64ronline_compat.c
+++ src/z64ronline_compat.c
@@ -3,9 +3,9 @@
 
 #include "apcommon.h"
 
-RECOMP_DECLARE_EVENT(Z64ROnline_onRandoLocationSent(u32 location, int slot));
+RECOMP_DECLARE_EVENT(Z64ROnline_onRandoLocationSent(u32 location));
 
 void Z64ROnline_rando_send_location(u32 location) {
     rando_send_location(location);
-    Z64ROnline_onRandoLocationSent(location, rando_get_own_slot_id());
+    Z64ROnline_onRandoLocationSent(location);
 }
